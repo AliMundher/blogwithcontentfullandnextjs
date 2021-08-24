143ca986a9e5f588e0c0888ef777bebcfa465fbd
Ali <mundhersaeedai@gmail.com>
2021-08-24 13:37:14 +0200

    contact nextjs to contentfull for get all data

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function header() {
             <div className="container">
                 <a className="navbar-brand" href="#">Logo</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import Head from 'next/head'
+import { createClient } from 'contentful';
 
 
-export default function Home() {
+export default function Home({ blogs }) {
+  console.log(blogs)
   return (
     <div className="container">
       <h1 className="btn btn-primary">home</h1>
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
+  })
+
+  const res = await client.getEntries({ content_type: 'blog' });
+
+  return {
+    props: {
+      blogs: res.items
+    }
+  }
 }
